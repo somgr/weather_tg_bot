faa36bcd0c96cc5f44cdeed837b6845fc3bf674b
somgr <deniska9767@gmail.com>
2022-08-13 11:23:10 +0500

    add weather condition

--- main.py
+++ main.py
@@ -5,29 +5,47 @@ from pprint import pprint
 from config import open_weather_token
 
 def get_weather(city, open_weather_token):
+
+    code_to_smile = {
+        "Clear": "Ясно",
+        "Clouds": "Облачно",
+        "Rain": "Дождь",
+        "Drizzle": "Дождь",
+        "Thunderstorm": "Гроза",
+        "Snow": "Снег",
+        "Mist": "Туман"
+    }
+
     try:
         r = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
         )
         data = r.json()
-
+        # pprint(data)
         city = data["name"]
         cur_weather = data["main"]["temp"]
+
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
+
+
         humidity = data["main"]["humidity"]
         pressure = data["main"]["pressure"]
         wind = data["wind"]["speed"]
         sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
         sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
-            data["sys"]["sunrise"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
 
-        print(f"Погода в городе {city}\nТемпература: {cur_weather}\n"
+        print(f"*** {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} ***\n"
+              f"Погода в городе {city}\nТемпература: {cur_weather}°C {wd}\n"
               f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст.\nВетер: {wind} м/с\n"
               f"Восход солнца: {sunrise_timestamp}\nЗакат солнца {sunset_timestamp}\n"
               f"Продолжительность дня: {length_of_the_day}\n"
               f"Хорошего дня"
               )
-
 
     except Exception as ex:
         print(ex)
